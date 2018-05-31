efe303bf6a63543f70542dd8d202119a049f7f37
Joanna Kolodziejska <j.baranowska@gmail.com>
2018-05-31 21:13:28 +0200

    week1

--- src/test/java/pl/cschool/week1/HistogramTest.java
+++ src/test/java/pl/cschool/week1/HistogramTest.java
@@ -2,18 +2,25 @@ package pl.cschool.week1;
 
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Random;
+
 import static org.junit.Assert.*;
 
 public class HistogramTest {
 
     @Test
     public void makeDistributionTest() {
-        //Given
+        int[] arr = generateArr(100, 30);
+        Histogram.printMap(Histogram.makeDistribution(arr));
+    }
 
-        //When
-
-        //Then
-
-        //assertEquals();
+    static int[] generateArr(int listSize, int range) {
+        Random rnd = new Random();
+        int[] myList = new int[listSize];
+        for (int i =0; i< listSize; i++) {
+            myList[i]=rnd.nextInt(range);
+        }
+        return myList;
     }
 }
--- src/test/java/pl/cschool/week1/PascalTriangleTest.java
+++ src/test/java/pl/cschool/week1/PascalTriangleTest.java
@@ -1,13 +1,16 @@
 package pl.cschool.week1;
 
 import org.junit.Test;
+import static pl.cschool.week1.PascalTriangle.*;
 
 import static org.junit.Assert.*;
 
 public class PascalTriangleTest {
 
     @Test
-    public void asdTest1() {
+    public void Test1() {
+
+        printTr(PascalTriangle.generateTr(10));
 
     }
 }
